9351374194fe824488485e19ef3a86a92d0d9d98
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2020-03-26 12:11:24 +0100

    fix gray screen when there aren't slices in slices page

--- lib/presentation/pages/slices_page.dart
+++ lib/presentation/pages/slices_page.dart
@@ -1,7 +1,7 @@
 import 'package:diary/domain/entities/motion_activity.dart';
 import 'package:flutter/material.dart';
 import 'package:diary/application/location_notifier.dart';
-import 'package:diary/application/root/date_notifier.dart';
+import 'package:diary/application/date_notifier.dart';
 import 'package:provider/provider.dart';
 
 class SlicesPage extends StatelessWidget {
@@ -9,7 +9,8 @@ class SlicesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final day = Provider.of<LocationNotifier>(context, listen: false).getDay();
     final slices = List.from(day.slices);
-    if (slices.last.activity == MotionActivity.Unknown) {
+
+    if (slices.isNotEmpty && slices.last.activity == MotionActivity.Unknown) {
       slices.removeLast();
     }
 
@@ -19,31 +20,35 @@ class SlicesPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: slices
-              .map(
-                (slice) => Card(
-                  child: ListTile(
-                    leading: Text(slice.id.toString()),
-                    title: Row(
-                      children: <Widget>[
-                        Text(
-                          slice.activity
-                              .toString()
-                              .replaceFirst('MotionActivity.', ''),
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 22),
-                        ),
-                        Text('  ->  '),
-                        Text(slice.formattedMinutes),
-                      ],
-                    ),
-                    subtitle: Text(slice.startTime.toString()),
-                  ),
-                ),
+        child: slices.isEmpty
+            ? Center(
+                child: Text('Non ci sono spicchi temporali per questo giorno'),
               )
-              .toList(),
-        ),
+            : ListView(
+                children: slices
+                    .map(
+                      (slice) => Card(
+                        child: ListTile(
+                          leading: Text(slice.id.toString()),
+                          title: Row(
+                            children: <Widget>[
+                              Text(
+                                slice.activity
+                                    .toString()
+                                    .replaceFirst('MotionActivity.', ''),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 22),
+                              ),
+                              Text('  ->  '),
+                              Text(slice.formattedMinutes),
+                            ],
+                          ),
+                          subtitle: Text(slice.startTime.toString()),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
       ),
     );
   }
