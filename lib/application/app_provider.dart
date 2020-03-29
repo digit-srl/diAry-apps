82b37e9286b222cbc6a4d8e82cd59dc14f7ddcdb
Riccardo <riccardo.maldini@gmail.com>
2020-03-30 01:20:03 +0200

    Android notification icon fix

--- lib/application/app_provider.dart
+++ lib/application/app_provider.dart
@@ -1,3 +1,4 @@
+import 'package:diary/utils/colors.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
     as bg;
@@ -23,18 +24,24 @@ class AppProvider with LocatorMixin {
     bg.BackgroundGeolocation.onEnabledChange(_onEnabledChange);
 
     bg.BackgroundGeolocation.ready(bg.Config(
-      reset: false,
-      debug: !kReleaseMode,
-      persistMode: bg.Config.PERSIST_MODE_ALL,
-      logLevel: bg.Config.LOG_LEVEL_VERBOSE,
-      desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
-      distanceFilter: 30.0,
-      stopTimeout: 1,
-      stopOnTerminate: false,
-      startOnBoot: true,
-      enableHeadless: false,
-      maxDaysToPersist: 30,
-    )).then((bg.State state) {
+        reset: false,
+        debug: !kReleaseMode,
+        persistMode: bg.Config.PERSIST_MODE_ALL,
+        logLevel: bg.Config.LOG_LEVEL_VERBOSE,
+        desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
+        distanceFilter: 30.0,
+        stopTimeout: 1,
+        stopOnTerminate: false,
+        startOnBoot: true,
+        enableHeadless: false,
+        maxDaysToPersist: 30,
+        notification: bg.Notification(
+          priority: -2, // bassa priorità: l'icona risulta meno invasiva (ridotta, quando possibile)
+          title: "Funzione di tracciamento attivata",
+          text: "L'app lavorerà in background senza disturbarti!",
+          smallIcon: "mipmap/notification_icon", // <-- defaults to app icon
+          largeIcon: "mipmap/notification_icon"
+        ))).then((bg.State state) {
       print("[ready] ${state.toMap()}");
       print(state.enabled);
       serviceNotifier.setEnabled(state.enabled);
