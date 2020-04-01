cea6d8202aa00db406b6dd46315e1e4fc9a2efaf
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2020-04-01 18:33:48 +0200

    duplicate functions for create slices (test) add reduceOnOff recursive function to process near slices in list

--- lib/utils/location_utils.dart
+++ lib/utils/location_utils.dart
@@ -41,9 +41,10 @@ class LocationUtils {
       Map<DateTime, List<bg.Location>> locationsPerDay) {
     Map<DateTime, Day> days = {};
     for (DateTime key in locationsPerDay.keys) {
-      final tmp = aggregateLocationsInSlices(locationsPerDay[key],
-          box:
-              Map<String, bool>.from(Hive.box<bool>('enabled_change').toMap()));
+//      final tmp = aggregateLocationsInSlices(locationsPerDay[key],
+//          box:
+//              Map<String, bool>.from(Hive.box<bool>('enabled_change').toMap()));
+      final tmp = aggregateLocationsInSlices2(locationsPerDay[key]);
       days[key] = Day(
           date: key,
           slices: tmp[0],
@@ -420,6 +421,311 @@ class LocationUtils {
     return [slices, places];
   }
 
+  static List<List<Slice>> aggregateLocationsInSlices2(
+      List<bg.Location> locations,
+      {List<Slice> partialDaySlices = const [],
+      List<Slice> partialDayPlaces = const []}) {
+    if (locations.isEmpty) return [[], []];
+
+    final currentDay = DateTime.tryParse(locations.first.timestamp).toLocal();
+
+    final List<Slice> slices = [];
+    final List<Slice> places = [];
+
+    Action lastAction = Action.Unknown;
+    bool isFirstGeofence = true;
+    String lastWhere;
+    slices.addAll(partialDaySlices);
+    places.addAll(partialDayPlaces);
+
+    final maxMinutes = 1440;
+    int cumulativeMinutes = partialDaySlices.isNotEmpty
+        ? partialDaySlices.last.startTime.toMinutes() +
+            partialDaySlices.last.minutes
+        : 0;
+
+    int cumulativePlacesMinutes = partialDayPlaces.isNotEmpty
+        ? partialDayPlaces.last.startTime.toMinutes() +
+            partialDayPlaces.last.minutes
+        : 0;
+
+    for (bg.Location loc in locations) {
+      final currentDate = DateTime.tryParse(loc.timestamp).toLocal();
+      final currentMinutes = currentDate.toMinutes();
+      final partialMinutes = currentMinutes - cumulativeMinutes;
+      final currentActivity = getActivityFromString(loc.activity.type);
+
+      int partialPlaceMinutes = currentMinutes - cumulativePlacesMinutes;
+
+      //gofence data
+      final geofence = loc.geofence;
+      final action = geofence?.action == null
+          ? Action.Unknown
+          : geofence?.action == 'EXIT' ? Action.Exit : Action.Enter;
+      final where = geofence?.identifier;
+      print('uuid: ${loc.uuid}, identifier: $where, action: $action');
+
+      //se places è vuoto aggiungo slice con place nullo o meno in base al geofence se exit o action
+      if (places.isEmpty) {
+        places.add(
+          Slice(
+            id: 0,
+            minutes: partialPlaceMinutes,
+            startTime: currentDate.withoutMinAndSec(),
+            places: action == Action.Unknown ? {} : {where},
+            activity: action == Action.Unknown
+                ? currentActivity
+                : MotionActivity.Still,
+            placeRecords: 1,
+          ),
+        );
+      } else if (loc.geofence == null) {
+        places.last.minutes += partialPlaceMinutes;
+        if (lastAction == Action.Enter) {
+          //ultima azione = Enter
+          places.last.placeRecords += 1;
+        } else if (lastAction == Action.Unknown) {
+          //ultima azione = Unknown
+          if (places.last.places.isEmpty) {
+            if (places.last.activity == currentActivity ||
+                places.last.activity == MotionActivity.Unknown) {
+              places.last.placeRecords += 1;
+            } else {
+              places.add(
+                Slice(
+                  id: 0,
+                  minutes: 0,
+                  activity: currentActivity,
+                  startTime: currentDate,
+                  places: {},
+                  placeRecords: 1,
+                ),
+              );
+            }
+          } else {
+            places.last.placeRecords += 1;
+          }
+        } else {
+          // ultima azione = EXIT
+          Set<String> newPlaces = Set.from(places.last.places);
+          if (newPlaces.contains(lastWhere)) {
+            newPlaces.remove(lastWhere);
+          }
+          if (places.last.places.isEmpty) {
+            if (places.last.activity == currentActivity ||
+                places.last.activity == MotionActivity.Unknown) {
+              places.last.activity = currentActivity;
+              places.last.placeRecords += 1;
+            } else {
+              places.add(
+                Slice(
+                  id: 0,
+                  minutes: 0,
+                  startTime: currentDate,
+                  places: newPlaces,
+                  activity: currentActivity,
+                  placeRecords: 1,
+                ),
+              );
+            }
+          } else {
+            places.last.placeRecords += 1;
+          }
+        }
+      } else {
+        //Geofence presente
+        places.last.minutes += partialPlaceMinutes;
+
+        if (action == Action.Enter) {
+          if (!places.last.places.contains(where)) {
+            Set<String> newPlaces = Set.from(places.last.places);
+            newPlaces.add(where);
+            places.add(
+              Slice(
+                id: 0,
+                minutes: 0,
+                startTime: currentDate,
+                places: newPlaces,
+                activity: MotionActivity.Still,
+                placeRecords: 1,
+              ),
+            );
+          } else {
+            places.last.placeRecords += 1;
+          }
+        } else if (action == Action.Exit) {
+          places.last.placeRecords += 1;
+          //situazione di primo EXIT della giornata visto che ol luogo non è contenuto nel precedente spicchio
+          if (isFirstGeofence) {
+            places.last.places.add(where);
+          }
+          //TODO potrebbe anche uscire da 2 posti di fila
+
+          Set<String> newPlaces = Set.from(places.last.places);
+          if (newPlaces.contains(where)) {
+            newPlaces.remove(where);
+          }
+
+          places.add(
+            Slice(
+              id: 0,
+              minutes: 0,
+              startTime: currentDate,
+              places: newPlaces,
+              activity: newPlaces.isEmpty
+                  ? MotionActivity.Unknown
+                  : MotionActivity.Still,
+              placeRecords: 0,
+            ),
+          );
+        }
+        isFirstGeofence = false;
+      }
+
+      lastAction = action;
+      lastWhere = where;
+      cumulativePlacesMinutes = currentMinutes;
+
+      if (slices.isEmpty) {
+        slices.add(
+          Slice(
+            id: 0,
+            minutes: partialMinutes,
+            activity: MotionActivity.Still,
+            startTime: currentDate.withoutMinAndSec(),
+          ),
+        );
+      } else if (slices.last.activity == currentActivity) {
+        slices.last.minutes += partialMinutes;
+      } else {
+        slices.last.minutes += partialMinutes;
+        slices.add(
+          Slice(
+            id: 0,
+            minutes: 0,
+            activity: currentActivity,
+            startTime: currentDate,
+          ),
+        );
+      }
+      cumulativeMinutes = currentMinutes;
+    }
+
+    print('cumulative minutes before last = $cumulativeMinutes');
+
+    if (cumulativePlacesMinutes < maxMinutes) {
+      if (currentDay.isToday()) {
+        places.add(
+          Slice(
+            id: 0,
+            minutes: maxMinutes - cumulativePlacesMinutes,
+            startTime: places.last.endTime,
+            places: {},
+          ),
+        );
+      } else {
+        places.last.minutes = maxMinutes - cumulativePlacesMinutes;
+      }
+      cumulativePlacesMinutes += maxMinutes - cumulativePlacesMinutes;
+    }
+
+    if (cumulativeMinutes < maxMinutes) {
+      if (currentDay.isToday()) {
+        slices.add(
+          Slice(
+            id: 0,
+            minutes: maxMinutes - cumulativeMinutes,
+            activity: MotionActivity.Unknown,
+            startTime: slices.last.endTime,
+          ),
+        );
+      } else {
+        slices.last.minutes = maxMinutes - cumulativeMinutes;
+      }
+      cumulativeMinutes += maxMinutes - cumulativeMinutes;
+    }
+
+    print('cumulative minutes complete = $cumulativeMinutes');
+
+    return [slices, places];
+  }
+
+  static List<Slice> buildOnOffSlices(Map<String, bool> box,
+      {List<Slice> slices = const []}) {
+    final maxMinutes = 1440;
+    final output = <Slice>[];
+    final keys = box.keys.toList();
+    final List<bool> values = box.values.toList();
+    for (int i = 0; i < keys.length; i++) {
+      final date = DateTime.tryParse(keys[i]);
+      if (i == 0 && values[i]) {
+        final minutes = date.toMinutes();
+        output.add(
+          Slice(
+            startTime: date.withoutMinAndSec(),
+            minutes: minutes,
+            activity: MotionActivity.Off,
+          ),
+        );
+      } else if (i == keys.length - 1 && !values[i]) {
+        final minutes = maxMinutes - date.toMinutes();
+        output.add(
+          Slice(
+            startTime: date,
+            minutes: minutes,
+            activity: MotionActivity.Off,
+          ),
+        );
+      } else if (!values[i] && values[i + 1]) {
+        final nextDate = DateTime.tryParse(keys[i + 1]);
+        final minutes = nextDate.toMinutes() - date.toMinutes();
+        output.add(
+          Slice(
+            startTime: date,
+            minutes: minutes,
+            activity: MotionActivity.Off,
+          ),
+        );
+        i++;
+      }
+    }
+    output.addAll(slices);
+    output.sort((a, b) => a.startTime.compareTo(b.startTime));
+    return output;
+  }
+
+  static List<Slice> reduceOnOff(List<Slice> slices) {
+    int i = 0;
+    final output = <Slice>[];
+    final list = <Slice>[];
+
+    if (slices.isEmpty) {
+      return output;
+    }
+    if (slices.first.activity != MotionActivity.Off) {
+      output.add(slices.first);
+      final x = reduceOnOff(slices.sublist(1));
+      output.addAll(x);
+      return output;
+    }
+
+    while (i < slices.length && slices[i].activity == MotionActivity.Off) {
+      list.add(slices[i]);
+      i++;
+    }
+
+    Slice slice = list.reduce((Slice a, Slice b) => Slice(
+          minutes: a.minutes + b.minutes,
+          startTime: list.first.startTime,
+          activity: a.activity,
+        ));
+
+    output.add(slice);
+    if (i < slices.length) {
+      output.addAll(reduceOnOff(slices.sublist(i)));
+    }
+    return output;
+  }
 //  static List<Day> aggregateLocationRecords(List<bg.Location> locations) {
 //    if (locations.isEmpty) return [];
 //
